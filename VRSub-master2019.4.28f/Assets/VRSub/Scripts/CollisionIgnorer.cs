ab2c6b3ba30cd5417cd339c3e3d6a104ba15c050
YollerBoller <94723183+atsmith22@users.noreply.github.com>
2022-02-15 20:41:40 -0500

    created basic setup for new interactables, need configuring

---
+++ VRSub-master2019.4.28f/Assets/VRSub/Scripts/CollisionIgnorer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionIgnorer : MonoBehaviour
+{
+    [SerializeField] private List<Collider> Sources = new List<Collider>();
+    [SerializeField] private List<Collider> Targets = new List<Collider>();
+    
+    private void Awake(){
+        int SourcesSize = Sources.Count;
+        int TargetsSize = Targets.Count;
+        int i, j;
+        for(i = 0; i < SourcesSize; i++){
+            for(j = 0; j < TargetsSize; j++){
+                Physics.IgnoreCollision(Sources[i], Targets[i]);
+            }
+        }
+    }
+}
