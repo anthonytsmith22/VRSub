45e3a1da7ead0c4186188e2d5528afb284baece4
YollerBoller <94723183+atsmith22@users.noreply.github.com>
2022-04-03 14:48:11 -0400

    modified RPC call requirement in InteractableRPCController

--- VRSub-master2019.4.28f/Assets/VRSub/Scripts/InteractableRPCController.cs
+++ VRSub-master2019.4.28f/Assets/VRSub/Scripts/InteractableRPCController.cs
@@ -23,9 +23,18 @@ public class InteractableRPCController : NetworkBehaviour
 
         // If gameObject has moved or been rotated by atleast the marginForRPC, 
         // then we will issue a ClientRPC to update the transform across all clients
-        if(Mathf.Abs((currentFramePosition - lastFramePosition).magnitude) >= marginForRPC || 
-            Mathf.Abs((currrentFrameRotation - lastFrameRotation).magnitude) >= marginForRPC){
+        // if(Mathf.Abs((currentFramePosition - lastFramePosition).magnitude) >= marginForRPC || 
+        //     Mathf.Abs((currrentFrameRotation - lastFrameRotation).magnitude) >= marginForRPC){
                 
+        //     if(isServer){
+        //         RpcUpdateTransform(currentFramePosition, currrentFrameRotation, transform);
+        //     }else{
+        //         CmdUpdateTransform(currentFramePosition, currrentFrameRotation, transform);
+        //     }
+            
+        // }
+
+        if(currentFramePosition != lastFramePosition || currrentFrameRotation != lastFrameRotation){                 
             if(isServer){
                 RpcUpdateTransform(currentFramePosition, currrentFrameRotation, transform);
             }else{
